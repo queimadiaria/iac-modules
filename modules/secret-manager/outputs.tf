f3ed8664a5f2bd11bcffd5d6576fd119fd5e333a
samueltmlara <125142328+samueltmlara@users.noreply.github.com>
2026-08-21 11:53:12 -0300

    Fix module to use one role for all services

--- modules/secret-manager/outputs.tf
+++ modules/secret-manager/outputs.tf
@@ -1,14 +1,19 @@
 output "secret_arn" {
-  description = "ARN do secret criado no AWS Secrets Manager"
   value       = aws_secretsmanager_secret.this.arn
 }
 
 output "secret_name" {
-  description = "Nome completo do secret criado"
   value       = aws_secretsmanager_secret.this.name
 }
 
 output "policy_arn" {
-  description = "ARN da IAM Policy de leitura dos segredos"
   value       = aws_iam_policy.secrets_read.arn
+}
+
+output "shared_role_arn" {
+  value       = local.shared_role_arn
+}
+
+output "shared_role_name" {
+  value       = local.shared_role_name
 }
